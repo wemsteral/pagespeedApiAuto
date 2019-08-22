29b35baccbe90917573e49ee663971884039c66d
Alasdair <alasdair123@hotmail.co.uk>
2019-08-22 12:21:10 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+var pageSpeedApiKey = "";
+// ENTER YOURS HERE AS STRING
+
+var urls = ["https://www.google.com", "https://www.ebay.com"];
+// EXAMPLES
+
+var tabs = ["google", "ebay"];
+//EXAMPLES
+
+// URLS & TABS MUST BE ALIGNED, EQUAL AND IN SAME ORDER.
+
+function monitor() {
+  for (i = 0; i < urls.length; i++) {
+    var desktop = callPageSpeed("desktop", i);
+    var mobile = callPageSpeed("mobile", i);
+    var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    var sheet = spreadsheet.getSheetByName(tabs[i]);
+    sheet.appendRow([
+      Utilities.formatDate(new Date(), "GMT", "yyyy-MM-dd"),
+      desktop.id,
+      mobile.id,
+      desktop
+    ]);
+  }
+}
+
+function callPageSpeed(strategy, url) {
+  var pageSpeedUrl =
+    "https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=" +
+    urls[url] +
+    "&key=" +
+    pageSpeedApiKey +
+    "&strategy=" +
+    strategy;
+  var response = UrlFetchApp.fetch(pageSpeedUrl);
+  var json = response.getContentText();
+  return JSON.parse(json);
+}
